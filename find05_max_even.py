b8ebbb58efb9e54ada5d35434b35a742e2f1bff3
abdusalom <boriboyevabdusalom@gmail.com>
2023-11-30 21:29:35 +0500

    5

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,4 +5,9 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    return 0
+    a=[x for x in data if x%2==0]
+    if not a:
+        return 0
+    return max(a)
+print(find_max_even([1, 4, 3, 8, 5]))
+print(find_max_even([7, 6, 3, 4, 9]))
